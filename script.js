367d99485826a2be22d45c94e6e4296dda89b2b6
Nikita Mitroshin <nikita.mitroshin@guidance.com>
2021-07-09 20:11:46 +0300

    Eslint fix

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 function runningAverage() {
-  let list = [];
+  const list = [];
   return function averageValue(item) {
     list.push(item);
     return list.reduce((a, b) => a + b, 0) / list.length;
   };
 }
 
-rAvg = runningAverage();
+const rAvg = runningAverage();
 console.log(rAvg(10));
 console.log(rAvg(11));
 console.log(rAvg(12));
-
-
-
